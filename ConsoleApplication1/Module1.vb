883291d30d8fc914cacf3625e9234215ff964f76
a245714555 <a245714555@gmail.com>
2015-10-01 20:55:05 +0800

    作業1

---
+++ ConsoleApplication1/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim n, sum, i As Integer
+        Console.WriteLine("=求1到100可以被n整除的整數=")
+        Console.WriteLine()
+        Console.WriteLine("請輸入1到100的整數n:")
+        n = Console.ReadLine
+        If n >= 1 And n <= 100 Then
+            Do While sum = n <= 100
+                sum +=
+            Loop
+
+        End If
+
+
+    End Sub
+
+End Module
